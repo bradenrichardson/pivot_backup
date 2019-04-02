085ae2e94e70578b5269b233eeabc31f955fc9dc
bradenrichardson <braden.richardson13@gmail.com>
2019-04-02 14:19:49 +0800

    Initial GUI design underway

--- pivot_backup_v2.0.py
+++ pivot_backup_v2.0.py
@@ -1,11 +1,17 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text('What is your name')],
-            [sg.InputText()],
-            [sg.Button('Ok')]]
+layout = [[sg.Text('Source')],
+          [sg.InputText(), sg.FolderBrowse()],
+          [sg.Text('Destination')],
+          [sg.InputText(), sg.FolderBrowse()],
+          [sg.Text('Delete', size=(20, 1)), sg.Text('Copy')],
+          [sg.Checkbox('Files', size=(17, 1)), sg.Checkbox('If New')],
+          [sg.Checkbox('Folders')],
+          [sg.Text('Scheduling')],
+          [sg.Radio('Run Once', 'loss', size=(17, 1)), sg.Radio('Daily', 'loss'), sg.InputText(size=(12, 1))],
+          [sg.Radio('Hourly', 'loss', size=(17, 1)), sg.Radio('Interval', 'loss'), sg.InputText(size=(10, 1))],
+          [sg.Submit()]]
 
-window = sg.Window('Test Title').Layout(layout)
+window = sg.Window('Pivot Backup').Layout(layout)
 
 button, values = window.Read()
-
-sg.Popup('Hello {} welcome to PySimpleGUI'.format(values[0]))
